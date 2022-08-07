c9b13000a7806b98fb1fcd933f03b1da2dd2706e
DorothyRKirk <64913825+DorothyRKirk@users.noreply.github.com>
2022-08-07 17:31:03 -0400

    Add files via upload

---
+++ Chapter01/Chp1-Ex7.cpp
@@ -0,0 +1,28 @@
+// (c) Dorothy R. Kirk. All Rights Reserved.
+// Purpose: Review a simple function
+
+#include <iostream>
+
+using std::cin;    // preferred to: using namespace std;
+using std::cout;
+using std::endl;
+
+int Minimum (int a, int b)
+{
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int x = 0, y = 0;
+
+    cout << "Enter two integers: ";
+    cin >> x >> y;
+    cout << "The minimum is: " << Minimum(x, y) << endl; 
+	
+    return 0;
+}
+
